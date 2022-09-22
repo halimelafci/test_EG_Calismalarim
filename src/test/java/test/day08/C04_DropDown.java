4aecd76681f8b2a6fba5a84e26d85dc3f3909137
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-22 14:51:33 +0300

    22.09.2022

--- src/test/java/test/day08/C04_DropDown.java
+++ src/test/java/test/day08/C04_DropDown.java
@@ -1,10 +1,13 @@
 package test.day08;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -18,7 +21,6 @@ public class C04_DropDown {
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.get("https://www.amazon.com");
 
     }
 
@@ -28,8 +30,6 @@ public class C04_DropDown {
     }
 
 
-
-
     //● Bir class oluşturun: DropDown
     //●  https://the-internet.herokuapp.com/dropdown adresine gidin.
     //1.Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
@@ -37,5 +37,26 @@ public class C04_DropDown {
     //3.Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
     //4.Tüm dropdown değerleri(value) yazdırın
     //5. Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True , degilse False yazdırın.
+
+    @Test
+    public void test1(){
+
+        //●  https://the-internet.herokuapp.com/dropdown adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+
+        //1.Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+       WebElement option1= driver.findElement(By.xpath("//option[@value=\"1\"]"));
+       option1.click();
+        System.out.println(option1.getText());
+
+
+        // 2.Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
+
+        WebElement option2= driver.findElement(By.xpath("//option[@value=\"2\"]"));
+        option2.click();
+        System.out.println(option2.getText());
+
+
+    }
 
 }
