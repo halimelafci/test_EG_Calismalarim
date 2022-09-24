e20d12c7d764606ba6a7806b4dbbc9e74a41e390
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-24 14:14:28 +0300

    24.09.2022

--- src/test/java/test/day12/C06_FileExist.java
+++ src/test/java/test/day12/C06_FileExist.java
@@ -1,14 +1,48 @@
 package test.day12;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class C06_FileExist extends TestBase {
 
     @Test
     public void test1(){
-        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.home")); // (C:\Users\SarıSiyah) bilgisayarim
+        //"C:\Users\SarıSiyah\Downloads\test (2).txt" (indirilen dosyanin yolu)
 
-        driver.get("https://the-internet.herokuapp.com/upload");
+        /*
+        Masaustundeki bir dosya yolunun tum bilgisayarlarda sorunsuz calismasi icin
+        dosya yolunu ikiye ayiracagiz
+        1.herkesin bilgisayarinda farkli olan isin
+        bu kismi bilgisayardan Syste.getProperty("user.home") kodu ile alabiliriz
+       2.herkes icin ayni olan kisim
+        bu ksim 1. maddedeki yolun devami seklinde olur
+
+        // driver.get("https://the-internet.herokuapp.com/upload");
+
+         */
+
+
+
+        //ornek indirilenler picture dosyasi icin yol kaydedelim
+        String dosyaYoluDinamik=System.getProperty("user.home")+"\\Downloads\\test (2).txt";
+
+        String dinamikOlmayan=System.getProperty("C:\\Users\\SarıSiyah\\Downloads\\test (2).txt");
+        //dinamik olmayan yol kolay ama sadece bu bilgisayarda calisir.
+
+
+        System.out.println("dosya yolumuz : "+dosyaYoluDinamik);
+        System.out.println(Files.exists(Paths.get(dosyaYoluDinamik)));;
+
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYoluDinamik)));
+
+        System.out.println(System.getProperty("user.dir"));
+    //suanda icinde bulundugumuz dosyayi verir (C:\Users\SarıSiyah\IdeaProjects\TestENG_Halime)
+
+
     }
 }
