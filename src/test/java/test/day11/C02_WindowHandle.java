dd01fd2447863711d5661c9ecb23c45df3943271
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-24 13:42:53 +0300

    24.09.2022

--- src/test/java/test/day11/C02_WindowHandle.java
+++ src/test/java/test/day11/C02_WindowHandle.java
@@ -1,13 +1,16 @@
 package test.day11;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
 
 public class C02_WindowHandle {
     static WebDriver driver;
@@ -36,9 +39,39 @@ public class C02_WindowHandle {
         //●  Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
         //●  Sayfadaki textin “New Window” olduğunu doğrulayın.
         //●  Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu doğrulayın.
-        //kullandığımız kodları içermektedir.
 
+
+
+
+
+        //●  https://the-internet.herokuapp.com/windows adresine gidin.
         driver.get("https://the-internet.herokuapp.com/windows");
+
+        //●  Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+        WebElement sayfadakiTxt=driver.findElement(By.xpath("//h3[text()='Opening a new window']"));
+
+        //●  Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
+        driver.getTitle().equals("The Internet");
+
+
+        //●  Click Here butonuna basın.
+        driver.findElement(By.xpath("//a[text()='Click Here']")).click();
+
+
+        //●  Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
+        ArrayList<String> windowHandles=new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandles.get(1));
+        driver.getTitle().equals("New Window");
+
+
+
+        //●  Sayfadaki textin “New Window” olduğunu doğrulayın.
+        driver.findElement(By.xpath("//title[text()='New Window']"));
+
+        //●  Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu doğrulayın.
+        driver.switchTo().window(windowHandles.get(0));
+        driver.getTitle().equals("The Internet");
+
 
 
 
